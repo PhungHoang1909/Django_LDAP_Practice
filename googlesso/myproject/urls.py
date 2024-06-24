2e4cb83a9e45f984cdc59508badcf249cead0789
phung hoang <lnphunghoang190903@gmail.com>
2024-06-24 11:07:28 +0700

    uploaded

--- googlesso/myproject/urls.py
+++ googlesso/myproject/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('myapp.urls')),
 
     path("google_sso/", include("django_google_sso.urls", namespace="django_google_sso")),
 ]
